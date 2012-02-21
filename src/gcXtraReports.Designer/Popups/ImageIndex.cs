e29c69287f33a48d2f0ba322e17caf1b148b2184
Jeremiah Redekop <jeremiah@geniuscode.net>
2012-02-20 18:31:44 -0800

    added missing files

---
+++ src/gcXtraReports.Designer/Popups/ImageIndex.cs
@@ -0,0 +1,11 @@
+using System.Linq;
+
+namespace GeniusCode.XtraReports.Designer.Popups
+{
+    public enum ImageIndex : int
+    {
+        Folder = 0,
+        DataSource,
+        Unavailable
+    }
+}
